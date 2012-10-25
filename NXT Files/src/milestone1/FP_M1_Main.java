add8ac74f0bb6c4fffbf5359eb96bc306c772f7e
nate.kb <nate.kb@quail.ieor.berkeley.edu>
2012-10-25 15:30:36 -0700

    Maybe done?

--- NXT Files/src/milestone1/FP_M1_Main.java
+++ NXT Files/src/milestone1/FP_M1_Main.java
@@ -1,5 +1,9 @@
 package milestone1;
 
+import lejos.nxt.Button;
+import lejos.nxt.Motor;
+import lejos.robotics.navigation.DifferentialPilot;
+
 public class FP_M1_Main {
 
 	/**
@@ -7,6 +11,45 @@ public class FP_M1_Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		double leftWheelDiameter = 5.42; // 5.57
+		double rightWheelDiameter = 5.44; //5.59
+		double trackWidth = 13.72; // 13.4
+
+
+
+		DifferentialPilot dp = new DifferentialPilot(leftWheelDiameter,
+				rightWheelDiameter, trackWidth, Motor.A, Motor.C, false);
+		
+		dp.setAcceleration(1500);
+		dp.setTravelSpeed(30);
+		dp.setRotateSpeed(360);
+
+		Button.waitForAnyPress();
+
+		dp.travel(488); //travel 488 cm = 4.88 m = 16 tiles
+
+		Button.waitForAnyPress();
+
+		int numRotations = 4;
+		for (int i = 0; i < numRotations; i++) {
+			dp.rotate(90);
+		}
+		
+		Button.waitForAnyPress();
+		
+		for (int i = 0; i < numRotations; i++) {
+			dp.rotate(-90);
+		}
+		
+		Button.waitForAnyPress();
+		
+		int sides = 4;
+		for (int i = 0; i < sides; i++) {
+			dp.travel(120);
+			dp.rotate(90);
+		}
+
 
 	}
 
